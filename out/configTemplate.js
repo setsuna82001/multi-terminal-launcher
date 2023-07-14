67218d3b2078ecd565945a43229083dc745a0d3b
setsuna82001 <setsuna82001@users.noreply.github.com>
2023-07-14 14:00:29 +0900

    Update configTemplate.js

--- out/configTemplate.js
+++ out/configTemplate.js
@@ -1,48 +1,60 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const template = [
-    {
-        label: "config #1",
-        commands: [
-            {
-                name: "first terminal",
-                main: true,
-                script: "script to run in terminal",
-                cwd: "<optional> A path or Uri for the current working directory to be used for the terminal."
-            },
-            {
-                name: "second terminal",
-                script: "script to run in terminal",
-                cwd: "<optional> A path or Uri for the current working directory to be used for the terminal."
-            },
-            {
-                name: "third terminal",
-                script: "script to run in terminal",
-                cwd: "<optional> A path or Uri for the current working directory to be used for the terminal."
-            }
-        ]
-    },
-    {
-        label: "config #2 <optional>",
-        commands: [
-            {
-                name: "first terminal",
-                main: true,
-                script: "script to run in terminal",
-                cwd: "<optional> A path or Uri for the current working directory to be used for the terminal."
-            },
-            {
-                name: "second terminal",
-                script: "script to run in terminal",
-                cwd: "<optional> A path or Uri for the current working directory to be used for the terminal."
-            },
-            {
-                name: "third terminal",
-                script: "script to run in terminal",
-                cwd: "<optional> A path or Uri for the current working directory to be used for the terminal."
-            }
-        ]
-    }
+  {
+    "label": "config (#1)",
+    "commands": [
+      {
+        "name": "first terminal",
+        "main": true, //<optional>
+        "script": "script to run in terminal",
+        "cwd": "<optional> A path or Uri for the current working directory to be used for the terminal.",
+        "color": "<optional> A terminal icon color. Refer to the URL -> https://code.visualstudio.com/api/references/theme-color#integrated-terminal-colors"
+        "icon": "<optional> A terminal icon."
+      },
+      {
+        "name": "second terminal",
+        "script": "script to run in terminal",
+        "cwd": "<optional> A path or Uri for the current working directory to be used for the terminal.",
+        "color": "<optional> A terminal icon color. Refer to the URL -> https://code.visualstudio.com/api/references/theme-color#integrated-terminal-colors"
+        "icon": "<optional> A terminal icon."
+      },
+      {
+        "name": "third terminal",
+        "script": "script to run in terminal",
+        "cwd": "<optional> A path or Uri for the current working directory to be used for the terminal.",
+        "color": "<optional> A terminal icon color. Refer to the URL -> https://code.visualstudio.com/api/references/theme-color#integrated-terminal-colors"
+        "icon": "<optional> A terminal icon."
+      }
+    ]
+  },
+  {
+    "label": "config (#2) <optional>",
+    "commands": [
+      {
+        "name": "first terminal",
+        "main": true, //<optional>
+        "script": "script to run in terminal",
+        "cwd": "<optional> A path or Uri for the current working directory to be used for the terminal.",
+        "color": "<optional> A terminal icon color. Refer to the URL -> https://code.visualstudio.com/api/references/theme-color#integrated-terminal-colors"
+        "icon": "<optional> A terminal icon."
+      },
+      {
+        "name": "second terminal",
+        "script": "script to run in terminal",
+        "cwd": "<optional> A path or Uri for the current working directory to be used for the terminal.",
+        "color": "<optional> A terminal icon color. Refer to the URL -> https://code.visualstudio.com/api/references/theme-color#integrated-terminal-colors"
+        "icon": "<optional> A terminal icon."
+      },
+      {
+        "name": "third terminal",
+        "script": "script to run in terminal",
+        "cwd": "<optional> A path or Uri for the current working directory to be used for the terminal.",
+        "color": "<optional> A terminal icon color. Refer to the URL -> https://code.visualstudio.com/api/references/theme-color#integrated-terminal-colors"
+        "icon": "<optional> A terminal icon."
+      }
+    ]
+  }
 ];
 exports.default = template;
 //# sourceMappingURL=configTemplate.js.map
